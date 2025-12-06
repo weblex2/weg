f2adeb07bf77b7857d3e0e0e8e885d6eefd3f516
Alex Noppenberger <noppenbe@gmx.de>
2025-12-06 02:37:59 +0100

    Update dashboard.blade.php

--- resources/views/homeassistant/dashboard.blade.php
+++ resources/views/homeassistant/dashboard.blade.php
@@ -581,7 +581,7 @@
         }
 
         function updateLightControls(entityId, containerId) {
-            fetch('/homeassistant:8123/state/' + encodeURIComponent(entityId))
+            fetch('/homeassistant/state/' + encodeURIComponent(entityId))
                 .then(response => response.json())
                 .then(data => {
                     if (data.success && data.state.state === 'on') {
